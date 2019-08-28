3239f27adf979e6900cf56655d5d0292332ce720
Jay Fallon <jayfallon@gmail.com>
2019-08-28 11:29:13 -0400

    commiting initial config

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import App, { Container } from 'next/app';
+import Head from 'next/head';
+import Page from '../components/Page';
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps, router } = this.props;
+
+    return (
+      <>
+        <Container>
+          <Page>
+            <Component {...pageProps} router={router} />
+          </Page>
+        </Container>
+      </>
+    );
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Home = () => (
+  <div>
+    <h1>My portfolio is a design system</h1>
+  </div>
+);
+
+export default Home;
